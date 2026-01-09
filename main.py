f0bbe7e39221d8e8c7aef8f2a0e73d9373e29683
Pritam Mondal <pritam987@gmail.com>
2026-01-09 17:32:05 +0530

    Add Tic Tac Toe game

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+window = width, height = 600, 400
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+screen = pygame.display.set_mode(size=window)
+
+# Fonts
+medium = pygame.font.Font("OpenSans-Regular.ttf", 28)
+
